7b715c99d6fcff6dd3128cae844fe535c43eed4f
smiley16479 <smiley16479@aol.com>
2024-08-04 23:15:33 +0200

    ajout de composant de sign-in ajout de composant de background

---
+++ front/src/lib/component/background/background.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+const imgUrl = '/MatchaFrida.webp';
+</script>
+
+<div style="background-image: url({imgUrl});">
+	<slot/>
+</div>
+<style>
+
+div {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  background-image: url({{imgUrl}}); /* Ne marche pas  */
+  background-size: cover; 
+  overflow: hidden;  /* Empêche le défilement dû à l'image ( !! mais cache ce qui en est en dehors !! )*/
+}
+
+</style>
