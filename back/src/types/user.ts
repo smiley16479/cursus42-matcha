b5b1b8854493373735997a671abfd9c66f3e8533
smiley16479 <smiley16479@aol.com>
2024-09-30 00:47:30 +0200

    ajout de la partie split des types

---
+++ back/src/types/user.ts
@@ -0,0 +1,30 @@
+import { RowDataPacket } from "mysql2";
+import {IUserInput, IUserOutput, IUserPictureInput} from './shared_type/user';
+
+export interface IUserDb extends RowDataPacket, IUserInput, IUserOutput {
+  createdAt: Date
+}
+
+export interface IEmailConfirmToken extends RowDataPacket {
+  id: number,
+  user: number,
+  confirmToken: string,
+  createdAt: Date
+}
+
+export interface IResetPasswordToken extends RowDataPacket {
+  id: number,
+  user: number,
+  resetToken: string,
+  createdAt: Date
+}
+
+export interface IUserInterest extends RowDataPacket {
+  id: number,
+  interest: string,
+  createdAt: Date
+}
+export interface IUserPicture extends RowDataPacket, IUserPictureInput {
+  id: number,
+  createdAt: Date
+}
