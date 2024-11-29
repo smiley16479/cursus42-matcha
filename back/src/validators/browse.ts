4cca5b3d265993f1bd0a0b8a3649d79f55f3c43f
fdeĉ <fd@42l.fr>
2024-11-29 18:11:17 +0100

    Fix error in browsing and research validation

--- back/src/validators/browse.ts
+++ back/src/validators/browse.ts
@@ -1,46 +1,46 @@
 import { body } from "express-validator";
 import { ESortingType, ESortOn } from "../types/shared_type/browse";
-import { EGender } from "../types/shared_type/user";
+import { ESexualPref } from "../types/shared_type/user";
 
 export const BrowseValidator = [
-    body("sexualPref").isString().trim().isIn(Object.values(EGender))
-    .withMessage(`Required Gender must be one of [${Object.values(EGender).join(', ')}]`),
+    body("sexualPref").isString().trim().isIn(Object.values(ESexualPref))
+        .withMessage(`Required Gender must be one of [${Object.values(ESexualPref).join(', ')}]`),
 
-    body("matchAgeMin").isInt({gt: -1})
-    .withMessage('Min age must be positive integer'),
+    body("matchAgeMin").isInt({ gt: -1 })
+        .withMessage('Min age must be positive integer'),
 
-    body("matchAgeMax").isInt({gt: -1})
-    .withMessage('Max age must be positive integer'),
+    body("matchAgeMax").isInt({ gt: -1 })
+        .withMessage('Max age must be positive integer'),
 
-    body("minFameRate").isInt({gt: -1})
-    .withMessage('Min famerate must be positive integer'),
+    body("minFameRate").isInt({ gt: -1 })
+        .withMessage('Min famerate must be positive integer'),
 
-    body("maxFameRate").isInt({gt: -1})
-    .withMessage('Max famerate must be positive integer'),
+    body("maxFameRate").isInt({ gt: -1 })
+        .withMessage('Max famerate must be positive integer'),
 
     body("latitude").toFloat().isFloat({ min: -90, max: 90 })
-    .withMessage('Location latitude must be between -90 and 90'),
-    
-    body("longitude").toFloat().isFloat({ min: -180, max: 180 })
-    .withMessage('Location longitude must be between -180 and 180'),
+        .withMessage('Location latitude must be between -90 and 90'),
 
-    body("maxDistance").isInt({gt: -1})
-    .withMessage('Max distance must be positive integer'),
+    body("longitude").toFloat().isFloat({ min: -180, max: 180 })
+        .withMessage('Location longitude must be between -180 and 180'),
+
+    body("maxDistance").isInt({ gt: -1 })
+        .withMessage('Max distance must be positive integer'),
 
     body("interests").isArray()
-    .withMessage('Interests must be an array of strings'),
+        .withMessage('Interests must be an array of strings'),
     body("interests.*").isString()
-    .withMessage('Interests must be an array of strings'),
+        .withMessage('Interests must be an array of strings'),
 
-    body("offset").isInt({gt: -1})
-    .withMessage('Offset must be positive integer'),
+    body("offset").isInt({ gt: -1 })
+        .withMessage('Offset must be positive integer'),
 
-    body("nbRequiredProfiles").isInt({gt: -1})
-    .withMessage('number of required profiles must be positive integer'),
-    
+    body("nbRequiredProfiles").isInt({ gt: -1 })
+        .withMessage('number of required profiles must be positive integer'),
+
     body("sortingOn").isString().trim().isIn(Object.values(ESortOn))
-    .withMessage(`Sorting on must be one of [${Object.values(ESortOn).join(', ')}]`),
+        .withMessage(`Sorting on must be one of [${Object.values(ESortOn).join(', ')}]`),
 
     body("sortingType").isString().trim().isIn(Object.values(ESortingType))
-    .withMessage(`Sorting type must be one of [${Object.values(ESortingType).join(', ')}]`),
+        .withMessage(`Sorting type must be one of [${Object.values(ESortingType).join(', ')}]`),
 ]
