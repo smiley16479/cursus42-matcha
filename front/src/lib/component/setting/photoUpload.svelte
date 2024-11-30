be27a2bcb97c044d4d3a603ddc0d8cebc333fc07
smiley16479 <smiley16479@aol.com>
2024-11-30 16:00:04 +0100

    remove du mock de photos

--- front/src/lib/component/setting/photoUpload.svelte
+++ front/src/lib/component/setting/photoUpload.svelte
@@ -1,50 +1,32 @@
-<script context="module">
-	import { writable } from 'svelte/store';
-  const photoUrls = writable([
-    {
-      filename: "https://via.placeholder.com/400x300",
-	    pictureIndex: 1
-    },
-    {
-      filename: "https://via.placeholder.com/400x300",
-	    pictureIndex: 2
-    },
-    {
-      filename: "https://via.placeholder.com/400x300",
-	    pictureIndex: 3
-    },
-    {
-      filename: "https://via.placeholder.com/400x300",
-	    pictureIndex: 4
-    },
-  ]);
-  let alreadyBeenMounted = false
-</script>
-
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { picUpload } from '@/service/user';
+  import { delAvatar, picUpload } from '@/service/user';
 	import { us } from '@/store/userStore';
-  import type { UserPic_t } from '@/type/shared_type/user'
-  // Initialisation d'un tableau pour stocker les photos (À CHANGER POULES VRAIS PHOTO)
-  let photos: File[] = [];
 
-  // Fonction pour gérer l'upload des photos
+  /** Fonction pour gérer l'upload des photos */
   async function handleFileUpload(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files) {
       for (let i = 0; i < input.files.length; i++) {
         const file = input.files[i];
-        photos.push(file);
         // Créer une URL pour prévisualiser l'image
         const url = URL.createObjectURL(file);
-        $photoUrls.push({filename: url, pictureIndex: $photoUrls.length});
-        $photoUrls = $photoUrls;
+        let pictureIndex = 0;
+        const idx = $us.user.pictures.map(e => e.pictureIndex);
+        for (let i = 1; i < 6; i++)
+          if (!idx.includes(i)) {
+            pictureIndex = i;
+            break;
+          }
+        if(!pictureIndex)
+          throw Error("Index de la photo erroné");
+        $us.user.pictures.push({filename: url, pictureIndex});
+        $us.user.pictures = $us.user.pictures;
 
         // Préparer les données pour l'envoi au backend
         const formData = new FormData();
         formData.append('picture', file);
-        formData.append('index', $photoUrls.length.toString()); 
+        formData.append('index', pictureIndex.toString()); 
 
         try {
           // Envoyer l'image au backend
@@ -59,51 +41,31 @@
     }
   }
 
-  async function handleFileChange(event : any) {
-    const fileInput = event.target;
-    const image = fileInput.files[0];
-    try {
-/*       const resp = await userService.txAvatar(formData, $us.user.userName);
-      console.log('handleFileChange', resp);
-      if (resp.status === 201)
-        console.log("Upload réussi", 'success'); */
-    } catch (err) {
-			// const error = err as AxiosError<any>;
-        console.log(`err`, err);
-    }
-    let reader = new FileReader();
-      reader.readAsDataURL(image);
-      reader.onload = e => {
-        // avatar = e.target!.result
-      };
-  }
-
   // Fonction pour supprimer une photo
-  function deletePhoto(index: number) {
+  async function deletePhoto(index: number, pictureIndex: number) {
     console.log(`delete photo index: ${index}`);
-    photos.splice(index, 1);
-    $photoUrls.splice(index, 1);
-    $photoUrls = $photoUrls;
+    try {
+      await delAvatar(pictureIndex);
+      $us.user.pictures.splice(index, 1);
+      $us.user.pictures = $us.user.pictures;
+    } catch (error) {
+      alert('photo non effacée');
+    }
   }
 
-  async function setFileAsUrl() {
-    for (const [i, e] of $us.pictures.entries()) {
+  /** Générer les thumbnail */
+  function setFileAsUrl() {
+    for (const [i, e] of $us.user.pictures.entries()) {
       if (!e.filename.includes('http://localhost'))
         e.filename = "http://localhost:3000/api/user/picture/" + e.filename
-      $photoUrls.push(e);
-      $photoUrls = $photoUrls;
     }
   }
 
-  // Nettoyer les URL à la destruction du composant
+  /** Nettoyer les URL à la destruction du composant */
   onMount(() => {
-    // Générer les thumbnail
-    if (!alreadyBeenMounted) {
-      alreadyBeenMounted = true;
-      setFileAsUrl();
-    }
+    setFileAsUrl();
     return () => {
-        $photoUrls.forEach((e) => URL.revokeObjectURL(e.filename));
+        $us.user.pictures.forEach((e) => URL.revokeObjectURL(e.filename));
     };
   });
 </script>
@@ -112,22 +74,22 @@
   <h2 class="text-2xl font-bold mb-4">Mes Photos</h2>
 
   <!-- Section d'upload des photos -->
-  {#if $photoUrls.length < 5}
+  {#if $us.user.pictures.length < 5}
     <input type="file" accept="image/*" multiple on:change={handleFileUpload} class="mb-4 p-2 border rounded" />
   {/if}
 
   <div class="grid grid-cols-2 gap-4">
-    {#each $photoUrls as elem, index}
+    {#each $us.user.pictures as elem, index}
       <div class="relative">
         <img src={elem.filename} alt="UploadedPic" class="w-32 h-32 object-cover rounded-md" />
-        <button on:click={() => deletePhoto(index)} class="absolute top-0 right-0 bg-red-500 text-white rounded-full p-1">
+        <button on:click={() => deletePhoto(index, elem.pictureIndex)} class="absolute top-0 right-0 bg-red-500 text-white rounded-full p-1">
           &times;
         </button>
       </div>
     {/each}
   </div>
 
-  {#if $photoUrls.length === 0}
+  {#if $us.user.pictures.length === 0}
       <p class="mt-4 text-gray-500">Aucune photo uploadée.</p>
   {/if}
 </div>
