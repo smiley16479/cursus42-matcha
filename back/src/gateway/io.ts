7699960527e0d6c49ad1dbec5f1498a1ca6bbb0f
smiley16479 <smiley16479@aol.com>
2024-12-01 15:32:47 +0100

    ajout JWT  middleware

--- back/src/gateway/io.ts
+++ back/src/gateway/io.ts
@@ -1,9 +1,34 @@
 import { Server, Socket } from 'socket.io';
-import type {Msg_t} from '../types/shared_type/msg'
+import type {Msg_t} from '../types/shared_type/msg';
+import { connectedUser, generateRoomId } from '../util/io.utils';
+import jwt from 'jsonwebtoken';
+import { getEnv } from '../util/envvars';
 
 export const initSocketEvents = (io: Server) => {
+
+    io.use((socket, next) => {
+        const authHeader = socket.handshake.headers.token;
+        if (!authHeader) {
+            console.error(`Middleware Socket.IO: No cookies sent`);
+            return next(new Error('No cookies sent'));
+        }
+
+        const token = (authHeader as string).split(' ')[1];
+        if (!token) {
+            return next(new Error('Unauthorized: No token provided'));
+        }
+
+        try {
+            const payload = jwt.verify(token, getEnv("JWT_SECRET")) as {id: number};
+            socket.user = payload;
+            next();
+        } catch (err) {
+            next(new Error('Unauthorized: Invalid token'));
+        }
+    });
+
     io.on('connection', (socket: Socket) => {
-        console.log('A user connected: ', socket.id);
+        console.log('A user connected: ', socket.user.id, "socket.handshake.headers", socket.handshake.headers.token);
 
     socket.on('c_joinRoom', (chatId) => {
         socket.join(chatId);
@@ -11,6 +36,7 @@ export const initSocketEvents = (io: Server) => {
     });
 
     socket.on('c_sendTxtMsg', (msg: Msg_t) => {
+        console.log(`c_sendTxtMsg received`);
         const { chatId, content, userId } = msg;
         const message = {
             chatId,
