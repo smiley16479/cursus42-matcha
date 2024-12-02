4df470ffd4f117b30f78f2db0311c1cc007becce
smiley16479 <smiley16479@aol.com>
2024-12-02 16:24:16 +0100

    ajout du component de recherche avancée et restructuration des routes de recherche de profil (browse)

---
+++ front/src/routes/app/frida/search/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Profil from "$lib/elem/profil/profil.svelte";
+import { page } from '$app/stores';
+	import { onMount } from "svelte";
+	import { visit } from "@/store/socketStore";
+	import { us } from "@/store/userStore";
+
+onMount(()=> {
+  console.log(`$page`, $page.params);
+  visit($us.user.id, parseInt($page.params.id));
+})
+</script>
+
+<!-- Consultation d'un profil -->
+<Profil userNum={parseInt($page.params.id)}/>
